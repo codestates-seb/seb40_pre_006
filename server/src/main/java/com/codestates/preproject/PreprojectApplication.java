d73e5fee4535e186b65044e6e349b47de28ac529
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2022-10-24 22:20:05 +0900

    #2 chore: 프로젝트 초기 세팅

---
+++ server/src/main/java/com/codestates/preproject/PreprojectApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.preproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreprojectApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/codestates/preproject/PreprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.preproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
