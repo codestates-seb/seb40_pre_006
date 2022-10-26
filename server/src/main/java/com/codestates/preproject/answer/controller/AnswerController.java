d9365ca4bcc1abccb79003e41c33de9b4230e8d6
jujang <jeongookjang@naver.com>
2022-10-26 18:42:08 +0900

    add answer, user, tag controller for apiTest

---
+++ server/src/main/java/com/codestates/preproject/answer/controller/AnswerController.java
@@ -0,0 +1,64 @@
+package com.codestates.preproject.answer.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/answer")
+public class AnswerController {
+
+    @PostMapping
+    public ResponseEntity postAnswer() {
+
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> map1 = new HashMap<>();
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        map1.put("answerId", "1");
+        map1.put("answerBody", "댓글 본문");
+        map1.put("createdAt", currentDateTime);
+        map1.put("name", "홍길동");
+        map1.put("questionId", "홍길동");
+
+        map.put("data", map1);
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{question-id}")
+    public ResponseEntity getAnswer(@PathVariable("question-id") long questionId){
+
+        Map<String, Object> map = new HashMap<>();
+        List<Object> list = new ArrayList<>();
+        Map<String, Object> map1 = new HashMap<>();
+        Map<String, Object> map2 = new HashMap<>();
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        map1.put("answerId", "1");
+        map1.put("answerBody", "댓글 본문");
+        map1.put("createdAt", currentDateTime);
+        map1.put("name", "홍길동1");
+        map1.put("questionId", "1");
+        map2.put("answerId", "2");
+        map2.put("answerBody", "댓글 본문");
+        map2.put("createdAt", currentDateTime);
+        map2.put("name", "홍길동2");
+        map2.put("questionId", "1");
+
+        list.add(map1);
+        list.add(map2);
+
+        map.put("data", list);
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+}
---
+++ server/src/main/java/com/codestates/preproject/user/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.codestates.preproject.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @PostMapping
+    public ResponseEntity postUser() {
+
+        Map<String, Object> map = new HashMap<>();
+        Map<String, String> map1 = new HashMap<>();
+
+        map1.put("userId", "1");
+        map1.put("email", "hgd@gmail.com");
+        map1.put("name", "홍길동");
+        map1.put("password", "123abc!@");
+        map1.put("questionCount", "0");
+
+        map.put("data", map1);
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping
+    public ResponseEntity getUsers() {
+
+        Map<String, Object> map = new HashMap<>();
+        List<Object> list = new ArrayList<>();
+        Map<String, String> map1 = new HashMap<>();
+        Map<String, String> map2 = new HashMap<>();
+
+        map1.put("userId", "1");
+        map1.put("name", "홍길동1");
+        map1.put("questionCount", "0");
+        map2.put("userId", "2");
+        map2.put("name", "홍길동2");
+        map2.put("questionCount", "0");
+        list.add(map1);
+        list.add(map2);
+
+        map.put("data", list);
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+}
