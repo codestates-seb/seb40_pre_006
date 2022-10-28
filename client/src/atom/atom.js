8894d885db26e9792cd0ce377e3487d5cf3e3a1c
ichenny <ichenny.lee@gmail.com>
2022-10-28 10:34:01 +0900

    #37 feat: 질문 작성 페이지 구현 중

--- client/src/atom/atom.js
+++ client/src/atom/atom.js
@@ -4,15 +4,15 @@ import { recoilPersist } from "recoil-persist";
 const { persistAtom } = recoilPersist();
 
 export const asideFocusState = atom({
-    key : 'asideFocusState',
-    default : '/',
-    effects_UNSTABLE: [persistAtom]
+  key: "asideFocusState",
+  default: "/",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const isAskState = atom({
-    key : 'isAskState',
-    default : '/'
-})
+  key: "isAskState",
+  default: "/",
+});
 
 export const headerToggleState = atom({
   key: "headerToggleState",
@@ -65,15 +65,29 @@ export const getDataState = atom({
   default: [],
 });
 
-
 export const questionOptionFocusState = atom({
-    key : 'questionOptionFocusState',
-    default : 1,
-    effects_UNSTABLE : [persistAtom]
-})
+  key: "questionOptionFocusState",
+  default: 1,
+  effects_UNSTABLE: [persistAtom],
+});
 
 export const questionCountState = atom({
-    key : 'questionCountState',
-    default : 0,
-    effects_UNSTABLE : [persistAtom]
-})
+  key: "questionCountState",
+  default: 0,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const questionTitleValueState = atom({
+  key: "questionTitleValuetState",
+  default: "",
+});
+
+export const questionContentValueState = atom({
+  key: "questionContentValueState",
+  default: "",
+});
+
+// export const questionTagValueState = atom({
+//   key: "questionTagValueState",
+//   default: "",
+// });
