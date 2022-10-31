3a76f9f417b986c6861a3e9a723554d57b6f5dec
ichenny <ichenny.lee@gmail.com>
2022-10-31 14:12:50 +0900

    #57 feat: 질문상세페이지 답변 기능 추가

--- client/src/pages/DetailQuestion.js
+++ client/src/pages/DetailQuestion.js
@@ -2,19 +2,19 @@ import styled from "styled-components";
 
 import QuestionTitle from "../components/DetailQuestionComp/QuestionTitle";
 import QuestionContent from "../components/DetailQuestionComp/QuestionContent";
+import YourAnswer from "../components/DetailQuestionComp/YourAnswer";
 
 const Container = styled.div`
   width: 70vw;
   min-height: 700px;
   padding-left: 1.5%;
 
-
   /* background-color : #ffcbcb; */
 
   @media screen and (max-width: 640px) {
     width: 100%;
     margin-right: 19px;
-    margin-left : 19px;
+    margin-left: 19px;
   }
 `;
 
@@ -24,6 +24,7 @@ const DetailQuestion = () => {
       <Container>
         <QuestionTitle />
         <QuestionContent />
+        <YourAnswer />
       </Container>
     </>
   );
