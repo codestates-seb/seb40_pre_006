660c29c9bd17ac0615c5e887d27eaf311013f621
jujang <jeongookjang@naver.com>
2022-11-01 12:11:09 +0900

    #85 feat: 예외처리 관련 ExceptionCode 추가

--- server/src/main/java/com/codestates/preproject/exception/ExceptionCode.java
+++ server/src/main/java/com/codestates/preproject/exception/ExceptionCode.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 public enum ExceptionCode {
     USER_NOT_FOUND(404, "User not found"),
     USER_EXISTS(409, "User exists"),
+    EMAIL_EXISTS(409, "Email exists"),
+    EMAIL_AND_USER_EXISTS(409, "Email and User exist"),
     ANSWER_NOT_FOUND(404, "Answer not found"),
     QUESTION_NOT_FOUND(404, "Question not found"),
     CANNOT_CHANGE_QUESTION(403, "Question can not change"),
