e718f1842694c268c0422523e093a75ad5ffd6e6
ichenny <ichenny.lee@gmail.com>
2022-11-02 13:15:15 +0900

    #102 style: 회원가입 페이지 UI 수정

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,18 +1,37 @@
 import styled from "styled-components";
 import SignUpForm from "../components/SignUpComponents/SignUpComp";
+import SignUpText from "../components/LoginComponents/SignUpText";
+import Oauth from "../components/LoginComponents/Oauth";
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: center;
   align-items: center;
+  background-color: #f1f2f3;
+
+  .text-container {
+    width: 50%;
+    /* border: 1px solid black; */
+  }
+
+  .signup-container {
+    width: 50%;
+    margin-left: 50px;
+    /* border: 1px solid red; */
+  }
 `;
 
 const SignUp = () => {
   return (
     <Container>
-      <SignUpForm />
+      <div className="text-container">
+        <SignUpText />
+      </div>
+      <div className="signup-container">
+        <Oauth />
+        <SignUpForm />
+      </div>
     </Container>
   );
 };
