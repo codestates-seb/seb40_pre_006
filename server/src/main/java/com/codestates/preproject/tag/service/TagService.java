e205bdcd5d1f5b6bae26300da18da61b74c5c8ba
wkqltm95 <wkqltm95@gmail.com>
2022-11-04 14:27:26 +0900

    #156 fix : getTagsRight 수정

--- server/src/main/java/com/codestates/preproject/tag/service/TagService.java
+++ server/src/main/java/com/codestates/preproject/tag/service/TagService.java
@@ -3,9 +3,14 @@ package com.codestates.preproject.tag.service;
 import com.codestates.preproject.tag.entity.Tag;
 import com.codestates.preproject.tag.repository.TagRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -22,6 +27,18 @@ public class TagService {
 
     public Tag updateTag(Tag tag) {
         return tagRepository.save(tag);
+    }
+
+    public Page<Tag> findTagsRight(int page, int size) {
+        List<Tag> list = tagRepository.findByTagCountGreaterThan(0)
+                .stream().sorted(Comparator.comparing(Tag::getTagCount).reversed())
+                .collect(Collectors.toList());
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+        int start = (int) pageRequest.getOffset();
+        int end = Math.min((start + pageRequest.getPageSize()), list.size());
+        Page<Tag> questionPage = new PageImpl<>(list.subList(start, end), pageRequest, list.size());
+        return questionPage;
     }
 
 }
